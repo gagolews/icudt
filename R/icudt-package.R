bb804071baf1801df024393c8358402387af1245
gagolews <egzaltowany@gmail.com>
2014-11-27 22:34:48 +0100

    Package metadata

---
+++ R/icudt-package.R
@@ -0,0 +1,53 @@
+## This file is part of the 'icudt' package for R.
+## Copyright (c) 2014, Marek Gagolewski
+## All rights reserved.
+##
+## Redistribution and use in source and binary forms, with or without
+## modification, are permitted provided that the following conditions are met:
+##
+## 1. Redistributions of source code must retain the above copyright notice,
+## this list of conditions and the following disclaimer.
+##
+## 2. Redistributions in binary form must reproduce the above copyright notice,
+## this list of conditions and the following disclaimer in the documentation
+## and/or other materials provided with the distribution.
+##
+## 3. Neither the name of the copyright holder nor the names of its
+## contributors may be used to endorse or promote products derived from
+## this software without specific prior written permission.
+##
+## THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+## "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING,
+## BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+## FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+## HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+## SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+## PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+## OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+## WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+## OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+## EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+
+#' @title ICU Data Files
+#'
+#' @description
+#' t.b.d.
+#'
+#'
+#' @name icudt-package
+#' @rdname icudt-package
+#' @docType package
+#' @author Marek Gagolewski \email{gagolews@@rexamine.com}\cr
+#' ICU4C was developed by IBM and others.
+#' The Unicode Character Database is due to Unicode, Inc.;
+#' see the COPYRIGHTS file for more details.
+#' @references
+#' \emph{\pkg{stringi} Package homepage}, \url{http://stringi.rexamine.com}
+#'
+#' \emph{ICU -- International Components for Unicode}, \url{http://www.icu-project.org/}
+#'
+#' \emph{ICU4C API Documentation}, \url{http://www.icu-project.org/apiref/icu4c/}
+#'
+#' \emph{The Unicode Consortium}, \url{http://www.unicode.org/}
+invisible(NULL)
